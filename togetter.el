35e9c42c0e530651fdd6d541a64ed8b353d9fded
Wataru MIYAGUNI <gonngo@gmail.com>
2012-08-10 00:17:53 +0900

    「残りを読む」ボタンを追加 #2

--- togetter.el
+++ togetter.el
@@ -32,18 +32,42 @@
 (require 'xml)
 (require 'helm)
 
-(defvar togetter->uri "http://togetter.com/li/")
-(defvar togetter->buffer-name "*Togetter*")
-(defvar togetter->hot-source-buffer-name "*Togetter Hot Source*")
-(defvar togetter->rec-source-buffer-name "*Togetter Recent Source*")
-(defvar togetter->tmp-buffer-name "*Togetter Temporary*")
-(defvar togetter->show-template "\
+(defvar togetter->more-loading-str "[残りを読み込み中...]")
+(defvar togetter->timeout 10
+  "Timeout time in seconds that `togetter:get-page'")
+
+(defconst togetter->base-uri "http://togetter.com"
+  "The base URI on Togetter.")
+(defconst togetter->api-uri (concat togetter->base-uri "/api/moreTweets")
+  "API URI that get more tweet") ;212550?page=
+(defconst togetter->buffer-name "*Togetter*"
+  "Buffer name to display tweets.")
+(defconst togetter->source-buffer-name "*Togetter Source*"
+  "`helm' source buffer name")
+(defconst togetter->tmp-buffer-name "*Togetter Temporary*"
+  "Temporary buffer name")
+(defconst togetter->show-template "\
 
 %s
 
 by [[%s][%s]] at [[%s][%s]]
 -----------------------------------------------------------------
+" "\
+Template view of tweet.
+
+1. tweet.
+2. link of author
+3. name of author
+4. link of tweet
+5. date of tweet
 ")
+
+;; Initialize
+;;
+;; TODO 実は変数宣言時に属性つけれたりする?
+(put-text-property 0 (length togetter->more-loading-str)
+                   'togetter-loading-str t
+                   togetter->more-loading-str)
 
 (defun togetter:lrtrim (text)
   (replace-regexp-in-string "\\`\\(?:\\s-\\|\n\\)+\\|\\(?:\\s-\\|\n\\)+\\'" "" text))
@@ -122,9 +146,79 @@ and convert to link string (org-mode format).
     (setq title (buffer-substring-no-properties begin end))
     (format "[[%s][%s]]" uri title)))
 
-(defun togetter:parse-more-box ()
-  ""
-)
+(defun togetter:search-text-with-property-in-current-buffer (prop)
+  (let (beg end)
+    (save-excursion
+      (goto-char (point-max))
+      (setq end (previous-single-property-change (point) prop))
+      (goto-char end)
+      (setq beg (previous-single-property-change (point) prop))
+      (unless beg
+        (setq beg end
+              end (point-max)))
+      (cons beg end))))
+
+(defun togetter:start-more-tweet-load (id page)
+  (let ((current-point (point))
+        (inhibit-read-only t)
+        (region (togetter:search-text-with-property-in-current-buffer
+                 'togetter-more-button))
+        (uri (concat togetter->api-uri "/" id "?page=" page))
+        xml tweet-items more)
+
+    ;; print loading message
+    (delete-region (car region) (cdr region))
+    (insert togetter->more-loading-str)
+
+    ;; loading start
+    (with-current-buffer togetter->tmp-buffer-name
+      (togetter:get-page uri togetter->tmp-buffer-name)
+      (setq xml (car (xml-parse-region (point-min) (point-max))))
+      (setq tweet-items (xml-get-children xml 'li))
+      (setq more (togetter:parse-more-tweet-box)))
+
+    ;; insert more tweets
+    (setq region (togetter:search-text-with-property-in-current-buffer
+                  'togetter-loading-str))
+    (delete-region (car region) (cdr region))
+    (insert (togetter:tweet-box-to-string tweet-items))
+    (when more (insert more))
+
+    (goto-char current-point)))
+
+(defun togetter:add-togetter-more-text-property (str _id _page)
+  (lexical-let ((id _id) (page _page))
+    (let ((map (make-sparse-keymap))
+          (func (lambda () (interactive) (togetter:start-more-tweet-load id page))))
+      (define-key map [return] func)
+      (define-key map [mouse-1] func)
+      (define-key map [down-mouse-1] 'mouse-set-point)
+      (add-text-properties 0 (length str)
+                           (list 'local-map map
+                                 'togetter-more-button t)
+                           str)
+      str)))
+
+(defun togetter:parse-more-tweet-box ()
+  "\
+Search div#more_tweet_box_xxxxx (xxxxx = id) in current buffer,
+and return text that load more tweet box in current buffer if click.
+
+If not found, return nil."
+  (let (begin end atag onclick loadstr page-id page-num)
+    (goto-char (point-min))
+    (when (search-forward "div id=\"more_tweet_box_" nil t)
+      (setq end (search-forward "</a>"))
+      (setq begin (search-backward "<a "))
+      (setq atag (car (xml-parse-region begin end)))
+
+      (setq loadstr (concat "[" (nth 2 atag) "]"))
+      (setq onclick (xml-get-attribute atag 'onclick))
+      (string-match "tgtr.moreTweets(\\([0-9]+\\),\\([0-9]+\\)" onclick)
+      (setq page-id  (match-string 1 onclick))
+      (setq page-num (match-string 2 onclick))
+
+      (togetter:add-togetter-more-text-property loadstr page-id page-num))))
 
 (defun togetter:get-page (uri &optional buffer)
   (if (null buffer) (setq buffer togetter->tmp-buffer-name))
@@ -133,7 +227,7 @@ and convert to link string (org-mode format).
       (erase-buffer)))
   (call-process "curl" nil `(,buffer nil) nil
                 "-f"
-                "-X" "GET" uri))
+                "-X" "GET" "-m" (number-to-string togetter->timeout) uri))
 
 (defun togetter:parse (uri &optional buffer)
   (if (null buffer) (setq buffer togetter->tmp-buffer-name))
@@ -141,18 +235,18 @@ and convert to link string (org-mode format).
     (togetter:get-page uri buffer)
     (list (togetter:parse-title uri)
           (togetter:parse-tweet-box)
-          (togetter:parse-more-box))))
+          (togetter:parse-more-tweet-box))))
 
 (defun togetter:show (title items more)
   (with-current-buffer (get-buffer-create togetter->buffer-name)
-      (setq buffer-read-only nil)
-      (erase-buffer)
-      (insert (concat title "\n\n"))
-      (insert (togetter:tweet-box-to-string items))
-      (goto-char (point-min))
-      (setq buffer-read-only t)
-      (org-mode)
-      (view-mode))
+    (erase-buffer)
+    (insert title "\n\n" (togetter:tweet-box-to-string items))
+    (org-mode)
+    (view-mode)
+    (when more
+      (let ((inhibit-read-only t))
+        (insert more)))
+    (goto-char (point-min)))
   (switch-to-buffer togetter->buffer-name))
 
 (defun togetter:run (uri)
@@ -164,7 +258,7 @@ and convert to link string (org-mode format).
 
 (defun togetter (id)
   (interactive "sTogetter id: ")
-  (togetter:run (concat togetter->uri id)))
+  (togetter:run (concat togetter->base-uri "/li/" id)))
 
 (defun togetter:make-rss-list (uri)
   (let (rss channel items)
@@ -192,7 +286,7 @@ and convert to link string (org-mode format).
                     ("View togetter" . (lambda (x) (togetter:run x)))
                     ("View togetter on browser"  . (lambda (x) (browse-url x)))
                     ))
-        :buffer togetter->hot-source-buffer-name
+        :buffer togetter->source-buffer-name
         ))
 
 (defun helm-togetter-recent ()
@@ -203,7 +297,7 @@ and convert to link string (org-mode format).
                     ("View togetter" . (lambda (x) (togetter:run x)))
                     ("View togetter on browser"  . (lambda (x) (browse-url x)))
                     ))
-        :buffer togetter->rec-source-buffer-name
+        :buffer togetter->source-buffer-name
         ))
 
 (provide 'togetter-el)
